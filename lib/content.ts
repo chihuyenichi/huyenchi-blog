60d5c4ec2ee19c59ade9311334551c4eaa4ed458
Huyen Chi <chihuyenichi@users.noreply.github.com>
2026-08-15 19:14:55 +0700

    fix: add base path to markdown reference links

--- lib/content.ts
+++ lib/content.ts
@@ -79,5 +79,6 @@ export function getRenderedMarkdown(post: Post) {
   const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
   return post.content
     .replace(/\]\(\.\/images\//g, `](${basePath}/images/posts/${post.slug}/`)
-    .replace(/\]\(\/(?!\/)/g, `](${basePath}/`);
+    .replace(/\]\(\/(?!\/)/g, `](${basePath}/`)
+    .replace(/^(\[[^\]]+\]:\s*)\/(?!\/)/gm, `$1${basePath}/`);
 }
